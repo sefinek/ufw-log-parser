3442889cc26efa720b4e1395aac77f64c1213236
Sefinek <contact@sefinek.net>
2025-04-28 08:33:55 +0200

    Update

--- examples/parseUfwLog.js
+++ examples/parseUfwLog.js
@@ -1,6 +1,6 @@
 const { parseUfwLog } = require('../index.js');
 
-const line = '2025-04-27T06:33:00.278063+02:00 sefinek-vps kernel: [UFW BLOCK] IN=ens3 OUT= MAC=00:d8:0f:6d:ab:20:50:87:89:68:26:73:08:00 SRC=172.70.240.4 DST=123.123.123 LEN=527 TOS=0x08 PREC=0x80 TTL=52 ID=36239 DF PROTO=TCP SPT=30962 DPT=443 WINDOW=12 RES=0x00 ACK PSH URGP=0';
-const parsed = parseUfwLog(line);
+const log = '2025-04-27T06:33:00.278063+02:00 sefinek-server kernel: [UFW BLOCK] IN=ens3 OUT= MAC=00:d8:0f:6d:ab:20:50:87:89:68:26:73:08:00 SRC=172.70.240.4 DST=104.21.23.5 LEN=527 TOS=0x08 PREC=0x80 TTL=52 ID=36239 DF PROTO=TCP SPT=30962 DPT=443 WINDOW=12 RES=0x00 ACK PSH URGP=0';
+const parsed = parseUfwLog(log);
 
 console.log(parsed);
